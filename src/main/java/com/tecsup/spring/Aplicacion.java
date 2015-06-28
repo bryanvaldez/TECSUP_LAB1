7558beaf8c933ea048e9f4f794648c38e65195ec
alumno <alumno@Lab1504-14>
2015-06-28 10:27:23 -0500

    Inicializando proyecto

---
+++ src/main/java/com/tecsup/spring/Aplicacion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tecsup.spring;
+
+/**
+ *
+ * @author alumno
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("tecsup");
+    }
+}
